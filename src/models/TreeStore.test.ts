3c416344aae9766f002d2c99f7808b4607502927
Aleksandr Vainshtein <asderq@gmail.com>
2025-06-06 19:37:24 +0200

    test: update expected output for getAll method in TreeStore tests

--- src/models/TreeStore.test.ts
+++ src/models/TreeStore.test.ts
@@ -87,7 +87,6 @@ describe("TreeStore with mixed id types", () => {
     const allItems = treeStore.getAll();
     expect(allItems).toEqual([
       { id: 1, parent: null, label: "Айтем 1" },
-
       { id: 2, parent: 1, label: "Айтем 2 - номер id" },
       { id: 3, parent: 1, label: "Айтем 3" },
     ]);
